52036cbc5c94814a83018e11a67c96bb45f67db9
abhi <programwithabhi@gmail.com>
2022-11-08 20:56:16 +0530

    Understand the global css and module css in the next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+// You can import the global css here that will apply every component
 
 // Every Component Will come here and render from here
 function MyApp({ Component, pageProps }) {
+  // console.log("Abhishek");
   return <Component {...pageProps} />
 }
 
