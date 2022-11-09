1fd0cbddb0cfb861936fd2434a968c372966db95
abhi <programwithabhi@gmail.com>
2022-11-10 04:36:37 +0530

    You can use the global css in the only _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 // You can import the global css here that will apply every component
+// In the component you can only add the module.css files 
 
 // Every Component Will come here and render from here
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   // console.log("Abhishek");
   return <Component {...pageProps} />
 }
 
-export default MyApp
+// export default MyApp
