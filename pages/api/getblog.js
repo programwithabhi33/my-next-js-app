ad5d86fd7791b0624e79690ba4e11de7df24ab63
abhi <programwithabhi@gmail.com>
2022-11-13 22:18:15 +0530

    Created two api to get the blogs files and corresponding file data api"s ready

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
-function abhishek(req, res) {
-    fs.readFile('./jsondata/abhi.json','utf-8',(err,data)=>{
+function abhi(req, res) {
+    fs.readFile(`jsondata/${req.query.slug}.json`,`utf8`,(err,data)=>{
+        // console.log(req.query.slug)
+        // console.log(data);
+        if (err){
+            res.status(500).json({error:"No such file found"});
+        }
         // console.log(typeof data)
         // const mainThing = JSON.parse(data)
         // console.log(mainThing)
-        // console.log(mainThing.name)
+        // console.log(mainThing.name
+        
         res.status(200).json(JSON.parse(data))
     });
 }
-export default abhishek;
+export default abhi;
   
